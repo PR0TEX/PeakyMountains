c084032c29cae8cd8627deca9b3fe5ab5cd632cd
PR0TEX <protex19@protonmail.com>
2022-12-03 20:32:52 +0100

    requirements.txt

--- scrappers/src/utils.py
+++ scrappers/src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def get_content(content):
     return content.text if content else ""
@@ -7,6 +8,11 @@ def create_link_with_query_param(base_url, page_category, query_param, param_val
     return base_url + page_category + "?" + query_param + "=" + param_value
 
 def save_data_to_csv(data, filename):
+    output_directory = "./data/"
     df = pd.DataFrame(data)
+
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+
     df.to_csv("./data/" + filename)
     print("Data saved to file "+filename)
